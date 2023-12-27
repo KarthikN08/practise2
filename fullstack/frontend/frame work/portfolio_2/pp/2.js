d494a73cff8ad569f49a41917366d7eef8bc9159
betlyves20 <betlyves20@gmail.com>
2023-12-27 23:38:41 +0530

    2

---
+++ fullstack/frontend/frame work/portfolio_2/pp/2.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    
+    var typed = new typed(".text", {
+      strings: ["Frontend Developer", "Graphic Designer", "Web Development", "Video Editor"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true
+    });
+  });
